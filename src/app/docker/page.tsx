b9288a7923e25ec088513b4114635f0090a3ccb5
Rhonal Chirinos <rhonal@antartida.ai>
2025-05-17 11:12:08 -0300

    mejoras

---
+++ src/app/docker/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Docker</h1>
+      <p>Docker is a platform for developers and sysadmins to develop, deploy, and run applications with containers.</p>
+      <p>
+        It allows you to package an application with all of its dependencies into a standardized unit for software
+        development.
+      </p>
+    </div>
+  )
+}
